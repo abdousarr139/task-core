ca4a1fda72f1317415accdf91b86cbd5c8a0fda4
abdousarr139 <abdousarr139@gmail.com>
2026-02-28 02:46:33 +0100

    feat: ajout de listTasks() - version 0.1.0-SNAPSHOT

--- src/main/java/sn/isi/l3gl/core/service/TaskService.java
+++ src/main/java/sn/isi/l3gl/core/service/TaskService.java
@@ -5,6 +5,8 @@ import sn.isi.l3gl.core.entity.Task;
 import sn.isi.l3gl.core.enums.TaskStatus;
 import sn.isi.l3gl.core.repository.TaskRepository;
 
+import java.util.List;
+
 @Service
 public class TaskService {
 
@@ -21,5 +23,9 @@ public class TaskService {
         task.setDescription(description);
         task.setStatus(TaskStatus.TODO);
         return taskRepository.save(task);
+    }
+    // VERSION 0.1.0 — listTasks()
+    public List<Task> listTasks() {
+        return taskRepository.findAll();
     }
 }
